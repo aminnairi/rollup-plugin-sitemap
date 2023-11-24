57777bdc027732c4a557fbae55fa9c9c953b7e8e
aniganesh <aniganesh741@gmail.com>
2023-11-25 00:10:53 +0530

    Fix issue with location urls

--- build/index.js
+++ build/index.js
@@ -2,8 +2,6 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-var path = require('path');
-
 const dateToYearString = (date) => {
     return date.getFullYear().toString();
 };
@@ -17,20 +15,32 @@ const sitemap = (options) => {
     return {
         name: "sitemap",
         generateBundle() {
-            const urlsTag = options.urls.map(url => {
-                const locationTag = `<loc>${path.join(options.baseUrl, url.location)}</loc>`;
-                const lastModifiedTag = url.lastModified ? `<lastmod>${dateToYearString(url.lastModified)}-${dateToMonthString(url.lastModified)}-${dateToWeekDateString(url.lastModified)}</lastmod>` : "";
-                const changeFrequencyTag = url.changeFrequency ? `<changefreq>${url.changeFrequency}</changefreq>` : "";
-                const priorityTag = url.priority ? `<priority>${url.priority}</priority>` : "";
+            const baseUrl = options.baseUrl.at(-1) === "/"
+                ? options.baseUrl
+                : options.baseUrl + "/";
+            const urlsTag = options.urls
+                .map((url) => {
+                const location = url.location[0] === "/" ? url.location.slice(1) : url.location;
+                const locationTag = `<loc>${baseUrl}${location}</loc>`;
+                const lastModifiedTag = url.lastModified
+                    ? `<lastmod>${dateToYearString(url.lastModified)}-${dateToMonthString(url.lastModified)}-${dateToWeekDateString(url.lastModified)}</lastmod>`
+                    : "";
+                const changeFrequencyTag = url.changeFrequency
+                    ? `<changefreq>${url.changeFrequency}</changefreq>`
+                    : "";
+                const priorityTag = url.priority
+                    ? `<priority>${url.priority}</priority>`
+                    : "";
                 return `<url>${locationTag}${lastModifiedTag}${changeFrequencyTag}${priorityTag}</url>`;
-            }).join("");
+            })
+                .join("");
             this.emitFile({
                 type: "asset",
                 name: "sitemap.xml",
                 fileName: "sitemap.xml",
-                source: `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urlsTag}</urlset>`
+                source: `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urlsTag}</urlset>`,
             });
-        }
+        },
     };
 };
 
--- sources/index.ts
+++ sources/index.ts
@@ -1,17 +1,16 @@
-import { Plugin } from "rollup"
-import { join } from "path"
+import { Plugin } from "rollup";
 
-export type ChangeFrequency 
-  = "always"
+export type ChangeFrequency =
+  | "always"
   | "hourly"
   | "daily"
   | "weekly"
   | "monthly"
   | "yearly"
-  | "never"
+  | "never";
 
-export type Priority
-  = 0
+export type Priority =
+  | 0
   | 0.1
   | 0.2
   | 0.3
@@ -21,56 +20,76 @@ export type Priority
   | 0.7
   | 0.8
   | 0.9
-  | 1
+  | 1;
 
 export interface Url {
-  location: string
-  lastModified?: Date
-  changeFrequency?: ChangeFrequency
-  priority?: Priority
+  location: string;
+  lastModified?: Date;
+  changeFrequency?: ChangeFrequency;
+  priority?: Priority;
 }
 
 export interface Options {
-  baseUrl: string,
-  urls: Array<Url>
+  baseUrl: string;
+  urls: Array<Url>;
 }
 
 interface Tag {
-  name: string
-  children: Array<Tag | null>
+  name: string;
+  children: Array<Tag | null>;
 }
 
 const dateToYearString = (date: Date): string => {
-  return date.getFullYear().toString()
-}
+  return date.getFullYear().toString();
+};
 
 const dateToMonthString = (date: Date): string => {
-  return (date.getMonth() + 1).toString().padStart(2, "0")
-}
+  return (date.getMonth() + 1).toString().padStart(2, "0");
+};
 
 const dateToWeekDateString = (date: Date): string => {
-  return date.getDate().toString().padStart(2, "0")
-}
+  return date.getDate().toString().padStart(2, "0");
+};
 
 export const sitemap = (options: Options): Plugin => {
   return {
     name: "sitemap",
     generateBundle() {
-      const urlsTag = options.urls.map(url => {
-        const locationTag = `<loc>${join(options.baseUrl, url.location)}</loc>`
-        const lastModifiedTag = url.lastModified ? `<lastmod>${dateToYearString(url.lastModified)}-${dateToMonthString(url.lastModified)}-${dateToWeekDateString(url.lastModified)}</lastmod>` : ""
-        const changeFrequencyTag = url.changeFrequency ? `<changefreq>${url.changeFrequency}</changefreq>` : ""
-        const priorityTag = url.priority ? `<priority>${url.priority}</priority>` : ""
+      const baseUrl =
+        options.baseUrl.at(-1) === "/"
+          ? options.baseUrl
+          : options.baseUrl + "/";
 
-        return `<url>${locationTag}${lastModifiedTag}${changeFrequencyTag}${priorityTag}</url>`
-      }).join("")
+      const urlsTag = options.urls
+        .map((url) => {
+          const location =
+            url.location[0] === "/" ? url.location.slice(1) : url.location;
+
+          const locationTag = `<loc>${baseUrl}${location}</loc>`;
+          const lastModifiedTag = url.lastModified
+            ? `<lastmod>${dateToYearString(
+                url.lastModified
+              )}-${dateToMonthString(url.lastModified)}-${dateToWeekDateString(
+                url.lastModified
+              )}</lastmod>`
+            : "";
+          const changeFrequencyTag = url.changeFrequency
+            ? `<changefreq>${url.changeFrequency}</changefreq>`
+            : "";
+          const priorityTag = url.priority
+            ? `<priority>${url.priority}</priority>`
+            : "";
+
+          return `<url>${locationTag}${lastModifiedTag}${changeFrequencyTag}${priorityTag}</url>`;
+        })
+        .join("");
 
       this.emitFile({
         type: "asset",
         name: "sitemap.xml",
         fileName: "sitemap.xml",
-        source: `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urlsTag}</urlset>`
-      })
-    }
-  }
-}
+        source: `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urlsTag}</urlset>`,
+      });
+    },
+  };
+};
